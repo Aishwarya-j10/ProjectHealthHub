0a84d6bd3ef184a5085fe4cf6d4ea83e272bb6e5
Aishwarya <jethani.aishwarya@gmail.com>
2023-09-05 14:40:58 +0530

    updated dashboard.html

--- app.py
+++ app.py
@@ -1,248 +1,192 @@
-# from flask import Flask, render_template, redirect, request, session, flash
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_bcrypt import Bcrypt
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+# from flask_migrate import Migrate
 
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a strong, secret key
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/admin/Downloads/Documents/FinalProject/site.db'
-
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-
-# # Create a User model for your database
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     password = db.Column(db.String(60), nullable=False)
-
-# @app.route('/')
-# def home():
-#     return render_template('index1.html')
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-
-#         # Hash the password before storing it in the database
-#         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
-#         # Create a new user and add it to the database
-#         user = User(username=username, password=hashed_password)
-#         db.session.add(user)
-#         db.session.commit()
-
-#         flash('Your account has been created! You can now log in.', 'success')
-#         return redirect('/login')
-#     return render_template('register.html', action='register')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-# #         username = request.form['username']
-# #         password = request.form['password']
-
-# #         # Query the database to find the user by username
-# #         user = User.query.filter_by(username=username).first()
-
-# #         if user and bcrypt.check_password_hash(user.password, password):
-# #             # Set the user in the session
-# #             session['user_id'] = user.id
-# #             flash('Login successful!', 'success')
-# #             return redirect('/dashboard')
-# #         else:
-# #             flash('Login unsuccessful. Please check your credentials.', 'danger')
-# #     return render_template('login.html', action='login')
-
-# # @app.route('/dashboard')
-# # def dashboard():
-# #     if 'user_id' in session:
-# #         user_id = session['user_id']
-# #         user = User.query.get(user_id)
-# #         return render_template('dashboard.html', user=user)
-# #     else:
-# #         return redirect('/login')
-
-# # @app.route('/logout')
-# # def logout():
-# #     session.pop('user_id', None)
-# #     return redirect('/')
-
-# # if __name__ == '__main__':
-# #     with app.app_context():
-# #         db.create_all()
-# #     app.run(debug=True)
-
-# from flask import Flask, render_template, redirect, url_for, flash, session, request
-# import csv 
-
-# app = Flask(__name__)
-# CSV_FILE = 'health_data.csv'
-
-# @app.route('/') 
-# def home() : 
-#     return render_template('index1.html') 
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         full_name = request.form['name']
-#         password = request.form['password']
-#         email = request.form['email']
-#         return redirect('/login')
-#     return render_template('login.html', action='register')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password'] 
-#         if username == 'your_username' and password == 'your_password':
-#             session['username'] = username  # Store the username in the session
-#             return redirect('/dashboard')  # Redirect after successful login
-#         else:
-#             return render_template('login.html', action='login', error='Invalid credentials')
-#     return render_template('login.html', action='login') 
-
-# @app.route('/dashboard')
-# def dashboard():
-#     # Check if the user is authenticated in the session
-#     if 'username' in session:
-#         username = session['username']
-#         return render_template('dashboard.html', username=username)
-#     else:
-#         return redirect('/login')  # Redirect to login if not authenticated
-    
-# @app.route('/dashboard/health', methods=['GET', 'POST'])
-# def health_dashboard():
-#     if 'username' not in session:
-#         return redirect('/login')
-
-#     if request.method == 'POST':
-#         username = session['username']
-#         date = request.form['date']
-#         weight = request.form['weight']
-#         # Add more fields as needed
-
-#         # Append the data to the CSV file
-#         with open(CSV_FILE, mode='a', newline='') as csv_file:
-#             csv_writer = csv.writer(csv_file)
-#             csv_writer.writerow([username, date, weight])
-
-#             # Read and display the user's health data from the CSV file
-#     username = session['username']
-#     health_data = []
-#     with open(CSV_FILE, mode='r') as csv_file:
-#         csv_reader = csv.reader(csv_file)
-#         for row in csv_reader:
-#             if row[0] == username:
-#                 health_data.append(row)
-
-#     return render_template('health_dashboard.html', health_data=health_data)
-
-# @app.route('/logout')
-# def logout():
-#     return redirect('/') 
-
-  
-# if __name__ == '__main__' : 
-#     app.run(debug = True) 
-
-from flask import Flask, render_template, redirect, flash, session, request
-import csv 
-from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.sql import func
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-CSV_FILE = 'health_data.csv'
-app.secret_key = 'your_secret_key_here'
-# Dummy user data (replace with database)
-users = [{'username': 'your_username', 'password_hash': 'your_password'}]
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/') 
-def home() : 
-    return render_template('index1.html') 
+app.secret_key = 'not_A_secret'
+# migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    phone_number = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<User {self.name}>' 
+    
+# Add a new table for health data
+class HealthData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('health_data', lazy=True))
+    age = db.Column(db.Integer)  
+    gender = db.Column(db.String) 
+    fever = db.Column(db.Boolean)
+    heart_rate = db.Column(db.Integer)
+    acidity = db.Column(db.Boolean) 
+    vomiting = db.Column(db.Boolean) 
+    headache = db.Column(db.Boolean) 
+    breathlessness = db.Column(db.Boolean)  
+    cough = db.Column(db.Boolean) 
+    anxiety = db.Column(db.Boolean) 
+
+    def __repr__(self):
+        return f'<HealthData for User {self.user_id}>'
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email, password=password).first()
+    if user:
+        session['user_id'] = user.id  
+        return render_template('dashboard.html', user = user)
+    else:
+        flash('Invalid email or password. Please try again.', 'danger')
+        return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        full_name = request.form['name']
-        password = request.form['password']
-        email = request.form['email']
-
-        # Check if the username is already taken
-        if any(user['username'] == username for user in users):
-            flash('Username is already taken. Please choose another.', 'error')
-        else:
-            # Store the registration data (in-memory for this example)
-            users.append({'username': username, 'password_hash': password})
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        phone_number = request.form.get('phone_number')
+        
+        new_user = User(name=name, email=email, password=password, phone_number=phone_number)
+        
+        try:
+            with app.app_context():
+                db.session.add(new_user)
+                db.session.commit()
             flash('Registration successful. You can now log in.', 'success')
-            return redirect('/login')
+            return redirect(url_for('home'))
+        except IntegrityError:
+            db.session.rollback()
+            flash('Email already registered. Please use a different email.', 'danger')
+    
+    return render_template('register.html') 
 
-    return render_template('login.html', action='register')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        user = next((user for user in users if user['username'] == username), None)
-
-        if user and check_password_hash(user['password_hash'], password):
-            session['username'] = username
-            flash('Login successful!', 'success')
-            return redirect('/dashboard')
-        else:
-            flash('Invalid credentials. Please try again.', 'error')
-
-    return render_template('login.html', action='login') 
 
 @app.route('/dashboard')
 def dashboard():
-    # if 'username' in session:
-    #     username = session['username']
-
-    #     # Read and display the user's health data from the CSV file
-        health_data = []
-        with open(CSV_FILE, mode='r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-    #         for row in csv_reader:
-    #             if row[0] == username:
-    #                 health_data.append(row)
-
-       # return render_template('dashboard.html', username=username, health_data=health_data)
-
-        return render_template('dashboard.html')
-    # else:
-    #     return redirect('/dashboard')
-
-@app.route('/dashboard/health', methods=['POST'])
-def health_dashboard():
-    if 'username' in session:
-        username = session['username']
-        date = request.form['date']
-        weight = request.form['weight']
-        # Add more fields as needed
-
-        # Append the data to the CSV file
-        with open(CSV_FILE, mode='a', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([username, date, weight])
-            
-        flash('Health data added successfully!', 'success')
-    else:
-        flash('You must be logged in to add health data.', 'error')
+    # Fetch the user's information from the session
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            return render_template('dashboard.html', user=user)
     
-    return redirect('/dashboard')
+    flash('You need to log in to access the dashboard.', 'danger')
+    return redirect(url_for('home'))
+
+
+@app.route('/add_health_data', methods=['POST'])
+def add_health_data():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            # Retrieve health data attributes from the form
+            age = int(request.form.get('age')) or None
+            gender = str(request.form.get('gender')) 
+            fever = bool(request.form.get('fever'))
+            heart_rate = int(request.form.get('heart_rate'))
+            # heart_rate = request.form.get('heart_rate') or None
+            # if heart_rate_str is None:
+            #     heart_rate = None
+            # else:
+            #     heart_rate = int(heart_rate_str)
+            acidity = bool(request.form.get('acidity')) 
+            vomiting = bool(request.form.get('vomiting')) 
+            headache = bool(request.form.get('headache')) 
+            breathlessness = bool(request.form.get('breathlessness')) 
+            cough = bool(request.form.get('cough')) 
+            anxiety = bool(request.form.get('anxiety')) 
+            # Add similar lines for other health data attributes
+            # lst = ['age', 'gender', 'fever', 'heart_rate', 'acidity', 'vomiting', 'headache', 'breathlessness', 'cough', 'anxiety'] 
+            # Set default values to None for attributes where no value is provided
+            # if age is None:
+            #     age = None
+            # if gender is None:
+            #     gender = None
+            
+            # if acidity is None:
+            #     acidity = None
+            # if vomiting is None:
+            #     vomiting = None
+            # if headache is None:
+            #     headache = None
+            # if breathlessness is None:
+            #     breathlessness = None
+            # if cough is None:
+            #     cough = None
+            # if anxiety is None:
+            #     anxiety = None
+            # Create a new HealthData record in the database
+            existing_health_data = HealthData.query.filter_by(user=user).first()
+            if existing_health_data : 
+                existing_health_data.age = age
+                existing_health_data.gender = gender
+                existing_health_data.fever = fever
+                existing_health_data.heart_rate = heart_rate
+                existing_health_data.acidity = acidity
+                existing_health_data.vomiting = vomiting
+                existing_health_data.headache = headache
+                existing_health_data.breathlessness = breathlessness
+                existing_health_data.cough = cough
+                existing_health_data.anxiety = anxiety
+                db.session.add(existing_health_data)
+                db.session.commit()
+            else : 
+                new_health_data = HealthData(
+                    user=user,
+                    age = age, 
+                    gender = gender, 
+                    fever=fever,
+                    heart_rate=heart_rate, 
+                    acidity = acidity, 
+                    vomiting = vomiting, 
+                    headache = headache, 
+                    breathlessness = breathlessness, 
+                    cough = cough, 
+                    anxiety = anxiety  
+                    # Add similar lines for other health data attributes
+                )
+                db.session.add(new_health_data)
+                db.session.commit()
+
+        flash('Health data submitted successfully.', 'success')
+        return redirect(url_for('dashboard', user = user))
+    else: 
+        flash('You need to log in to submit health data.', 'danger')
+        return redirect(url_for('home'))
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    return redirect('/') 
+    session.pop('user_id', None)  # Clear the user ID from the session
+    return redirect(url_for('home'))
 
-if __name__ == '__main__' : 
-    app.secret_key = 'your_secret_key'  # Change this to a secret key
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True) 

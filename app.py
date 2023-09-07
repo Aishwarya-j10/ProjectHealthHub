42f26ac5926d674eb87c71c70132b119fbdcd3cf
Aishwarya <jethani.aishwarya@gmail.com>
2023-09-07 18:34:34 +0530

    added visualizations for insights

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 import pandas as pd
-from io import StringIO
-# from flask_migrate import Migrate
+from datetime import datetime 
+from io import StringIO 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
 
 from sqlalchemy.sql import func
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,7 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.secret_key = 'not_A_secret'
-# migrate = Migrate(app, db)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,41 +31,37 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.name}>' 
     
-# Add a new table for health data
+
 class HealthData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('health_data', lazy=True))
     age = db.Column(db.Integer)  
-    gender = db.Column(db.String) 
-    fever = db.Column(db.Boolean)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow) 
+    gender = db.Column(db.String)
     heart_rate = db.Column(db.Integer)
-    acidity = db.Column(db.Boolean) 
-    vomiting = db.Column(db.Boolean) 
-    headache = db.Column(db.Boolean) 
-    breathlessness = db.Column(db.Boolean)  
-    cough = db.Column(db.Boolean) 
-    anxiety = db.Column(db.Boolean) 
+    anxiety = db.Column(db.Integer) 
 
     def __repr__(self):
-        return f'<HealthData for User {self.user_id}>'
+        return f'<HealthData for User {self.id}>'
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('start.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    if request.method == 'POST' :
+        email = request.form.get('email')
+        password = request.form.get('password')
     
-    user = User.query.filter_by(email=email, password=password).first()
-    if user:
-        session['user_id'] = user.id  
-        return render_template('dashboard.html', user = user)
-    else:
-        flash('Invalid email or password. Please try again.', 'danger')
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            session['user_id'] = user.id   
+            return redirect(url_for('dashboard')) 
+        else:
+            flash('Invalid email or password. Please try again.', 'danger')
+            return redirect(url_for('home')) 
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -89,95 +88,55 @@ def register():
 
 @app.route('/dashboard')
 def dashboard():
-    # Fetch the user's information from the session
+    
     user_id = session.get('user_id')
     if user_id:
         user = User.query.get(user_id)
         if user:
             return render_template('dashboard.html', user=user)
+         
+        
     
     flash('You need to log in to access the dashboard.', 'danger')
     return redirect(url_for('home'))
 
 
-@app.route('/add_health_data', methods=['POST'])
+
+@app.route('/add_health_data', methods=['GET', 'POST'])
 def add_health_data():
     user_id = session.get('user_id')
+
     if user_id:
         user = User.query.get(user_id)
         if user:
-            print(f'User ID: {user_id}')
-            # Retrieve health data attributes from the form
-            age = int(request.form.get('age')) if request.form.get('age') else None
-            gender = str(request.form.get('gender')) 
-            fever = bool(request.form.get('fever'))
-            heart_rate = int(request.form.get('heart_rate')) if request.form.get('heart_rate') else None
-            # heart_rate = request.form.get('heart_rate') or None
-            # if heart_rate_str is None:
-            #     heart_rate = None
-            # else:
-            #     heart_rate = int(heart_rate_str)
-            acidity = bool(request.form.get('acidity')) 
-            vomiting = bool(request.form.get('vomiting')) 
-            headache = bool(request.form.get('headache')) 
-            breathlessness = bool(request.form.get('breathlessness')) 
-            cough = bool(request.form.get('cough')) 
-            anxiety = bool(request.form.get('anxiety')) 
+            if request.method == 'GET':
+               
+                return render_template('enter_data.html')  
 
-            existing_health_data = HealthData.query.filter_by(user=user).first()
-            if existing_health_data : 
-                existing_health_data.user = user.id
-                existing_health_data.age = age
-                existing_health_data.gender = gender
-                existing_health_data.fever = fever
-                existing_health_data.heart_rate = heart_rate
-                existing_health_data.acidity = acidity
-                existing_health_data.vomiting = vomiting
-                existing_health_data.headache = headache
-                existing_health_data.breathlessness = breathlessness
-                existing_health_data.cough = cough
-                existing_health_data.anxiety = anxiety
-                db.session.add(existing_health_data)
-                db.session.commit()
-            else : 
-                new_health_data = HealthData(
-                    user=user.id,
-                    age = age, 
-                    gender = gender, 
-                    fever=fever,
-                    heart_rate=heart_rate, 
-                    acidity = acidity, 
-                    vomiting = vomiting, 
-                    headache = headache, 
-                    breathlessness = breathlessness, 
-                    cough = cough, 
-                    anxiety = anxiety  
-                    # Add similar lines for other health data attributes
+            elif request.method == 'POST':
+                datetime_str = request.form.get('datetime')
+                health_datetime = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M')
+                age = request.form.get('age')
+                gender = request.form.get('gender')
+                heart_rate = request.form.get('heart_rate')
+                anxiety = request.form.get('anxiety')
+
+                health_data = HealthData(
+                    user_id=user.id,
+                    datetime=health_datetime,
+                    age=age,
+                    gender=gender,
+                    heart_rate=heart_rate,
+                    anxiety=anxiety,
                 )
-                db.session.add(new_health_data)
+                db.session.add(health_data)
                 db.session.commit()
+                flash('Health data submitted successfully.', 'success')
+                return redirect(url_for('dashboard', user=user))
 
-        flash('Health data submitted successfully.', 'success')
-        return redirect(url_for('dashboard', user = user))
-    else: 
-        flash('You need to log in to submit health data.', 'danger')
-        return redirect(url_for('home'))
-    
-# @app.route('/view_data')
-# def view_data():
-#     user_id = session.get('user_id')
-#     if user_id:
-#         user = User.query.get(user_id) 
-#         # user = User.query.filter_by(user_id = user_id).first()
-#         print(user)
-#         if user:
-#             # Fetch the user's health data
-#             health_data = HealthData.query.filter_by(user_id=user_id).all()
-#             print(health_data)
-#             return render_template('view_data.html', user=user, health_data=health_data)
-    
-#     flash('You need to log in to view health data.', 'danger')
-#     return redirect(url_for('home'))
+    flash('You need to log in to submit health data.', 'danger')
+    return redirect(url_for('home'))
+ 
 @app.route('/view_data', methods=['GET'])
 def view_data():
     print("out of if") 
@@ -186,7 +145,7 @@ def view_data():
         user = User.query.get(user_id)
         if user:
             try:
-                # Fetch the user's health data
+                
                 health_data = HealthData.query.filter_by(user_id=user_id).all()
                 print("User:", user)
                 print("Health Data:", health_data)
@@ -198,15 +157,25 @@ def view_data():
 
     
     flash('You need to log in to view health data.', 'danger')
-    return redirect(url_for('home'))
+    return redirect(url_for('home'))   
 
-
-# import os
-# from flask import Flask, render_template, request, redirect, url_for, flash, session, Response
-# import pandas as pd
-
-
-# ... (your existing code)
+@app.route('/visualization', methods=['GET'])
+def visualizing():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            # try:
+                
+                health_data = HealthData.query.filter_by(user_id=user_id).all()
+                print("User:", user)
+                print("Health Data:", health_data)
+                image_path = [] 
+                image_path = visualizations()
+                return render_template('visualizations.html', user=user, health_data=health_data, image_path = image_path)
+   
+    flash('You need to log in to view health data.', 'danger')
+    return redirect(url_for('home'))  
 
 @app.route('/download_health_data_csv')
 def download_health_data_csv():
@@ -216,37 +185,24 @@ def download_health_data_csv():
         if user:
             health_data = HealthData.query.filter_by(user_id=user_id).all()
             if health_data:
-                # Create a list of dictionaries with the selected columns
+                
                 data_list = [
                     {
+                        "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
                         "Age": data.age,
                         "Gender": data.gender,
-                        "Fever": data.fever,
                         "Heart Rate": data.heart_rate,
-                        "Acidity": data.acidity,
-                        "Vomiting": data.vomiting,
-                        "Headache": data.headache,
-                        "Breathlessness": data.breathlessness,
-                        "Cough": data.cough,
-                        "Anxiety": data.anxiety,
+                        "Anxiety Level": data.anxiety,  
                     }
                     for data in health_data
                 ]
 
-                # Create a Pandas DataFrame from the list of dictionaries
+              
                 df = pd.DataFrame(data_list)
-
-                # Create a unique file name
                 file_name = f"user_{user.id}_health_data.csv"
-
-                # Save the DataFrame to a CSV file in memory
                 csv_data = StringIO()
                 df.to_csv(csv_data, index=False)
-
-                # Move the file pointer to the beginning of the file
                 csv_data.seek(0)
-
-                # Create a response with the CSV data
                 response = Response(
                     csv_data,
                     content_type="text/csv",
@@ -256,50 +212,78 @@ def download_health_data_csv():
                 )
                 return response
 
-    flash('No health data available for download.', 'danger')
-    return redirect(url_for('dashboard'))
+    return redirect(url_for('login'))
 
-# Add a new route to download the entire health_data table
+# @app.route('/visual_testing')
+# def visual_testing():
+#     user_id = session.get('user_id')
+#     if user_id:
+#         user = User.query.get(user_id)
+#         if user:
+#             health_data = HealthData.query.filter_by(user_id=user_id).all()
+#             if health_data:
+                
+#                 data_list = [
+#                     {
+#                         "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
+#                         "Age": data.age,
+#                         "Gender": data.gender,
+#                         "Heart Rate": data.heart_rate,
+#                         "Anxiety Level": data.anxiety,  
+#                     }
+#                     for data in health_data
+#                 ]
+
+              
+#                 df = pd.DataFrame(data_list)
+#                 file_name = f"user_{user.id}_health_data.csv"
+#                 csv_data = StringIO()
+#                 df.to_csv(csv_data, index=False)
+#                 csv_data.seek(0)
+#                 response = Response(
+#                     csv_data,
+#                     content_type="text/csv",
+#                     headers={
+#                         "Content-Disposition": f"attachment; filename={file_name}"
+#                     }
+#                 )
+#                 return response
+
+    # return redirect(url_for('login'))
+# Addding a new route to download the entire health_data table
 @app.route('/download_entire_health_data_csv')
 def download_entire_health_data_csv():
     user_id = session.get('user_id')
     if user_id:
         user = User.query.get(user_id)
         if user:
-            health_data = HealthData.query.all()  # Retrieve all health data
+            health_data = HealthData.query.all()  
             if health_data:
-                # Create a list of dictionaries with all columns
+               
                 data_list = [
                     {
-                        "User ID": data.user_id,
+                        "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
                         "Age": data.age,
                         "Gender": data.gender,
-                        "Fever": data.fever,
                         "Heart Rate": data.heart_rate,
-                        "Acidity": data.acidity,
-                        "Vomiting": data.vomiting,
-                        "Headache": data.headache,
-                        "Breathlessness": data.breathlessness,
-                        "Cough": data.cough,
-                        "Anxiety": data.anxiety,
+                        "Anxiety Level": data.anxiety, 
                     }
                     for data in health_data
                 ]
-
-                # Create a Pandas DataFrame from the list of dictionaries
+             
                 df = pd.DataFrame(data_list)
-
-                # Create a unique file name
+            
+                
                 file_name = "entire_health_data.csv"
 
-                # Save the DataFrame to a CSV file in memory
+                
                 csv_data = StringIO()
                 df.to_csv(csv_data, index=False)
 
-                # Move the file pointer to the beginning of the file
+                
                 csv_data.seek(0)
 
-                # Create a response with the CSV data
+                
                 response = Response(
                     csv_data,
                     content_type="text/csv",
@@ -309,14 +293,90 @@ def download_entire_health_data_csv():
                 )
                 return response
 
-    flash('No health data available for download.', 'danger')
-    return redirect(url_for('dashboard'))
+def visualizations():
+   
+    # df = pd.read_csv("user_1.csv") 
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            health_data = HealthData.query.all()  
+            if health_data:
+               
+                data_list = [
+                    {
+                        "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
+                        "Age": data.age,
+                        "Gender": data.gender,
+                        "Heart Rate": data.heart_rate,
+                        "Anxiety Level": data.anxiety, 
+                    }
+                    for data in health_data
+                ]
+             
+                df = pd.DataFrame(data_list) 
+    df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+    
+    # Filtering data for a specific month (September 2023)
+    start_date = '2023-09-01'
+    end_date = '2023-10-01'
+    df1 = df[(df['Date/Time'] >= start_date) & (df['Date/Time'] < end_date)] 
+    
+    # SORTING THE VALUES IN THE DATAFRAME AND RESETTING THE INDEX:
+    df1 = df1.sort_values(by='Date/Time').reset_index(drop=True)
+    df1.tail()
+    selected_columns = ['Heart Rate', 'Anxiety Level'] 
+    subset_data = df1[selected_columns] 
+    correlation_matrix = subset_data.corr()  
+    # CREATING A TIME SERIES PLOT FOR "Heart Rate"
+    plt.figure(figsize=(6, 3))
+    plt.plot(df1['Date/Time'], df1['Heart Rate'], marker='o', linestyle='-', color='b')
+    plt.title('Heart Rate Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Heart Rate')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()   
+    image_path = "static/plot.png"
+    plt.savefig(image_path)
+    
+    # Creating a histogram of anxiety levels for the month: 
+    plt.figure(figsize=(6, 3))
+    plt.hist(df1['Anxiety Level'], bins=10, edgecolor='y')
+    plt.title('Anxiety Level Distribution (September 2023)')
+    plt.xlabel('Anxiety Level')
+    plt.ylabel('Frequency')
+    plt.tight_layout()   
+    image_path1 = "static/plot1.png"
+    plt.savefig(image_path1) 
+    # Create a pie chart
+    category_counts = df1['Heart Rate'].value_counts()
+    plt.figure(figsize=(5, 5))
+    plt.pie(category_counts, labels=category_counts.index, autopct='%1.1f%%', startangle=140)
+    plt.title('Heart Rate Categories')
+    plt.axis('equal')  
+    image_path2 = "static/plot2.png"
+    plt.savefig(image_path2) 
+    selected_columns = ['Heart Rate', 'Anxiety Level'] 
+    subset_data = df1[selected_columns] 
+    correlation_matrix = subset_data.corr()
+
+    # CREATING A HEATMAP TO CHECK HOW ARE ANXIETY AND HEART RATE RELATED: 
+    plt.figure(figsize=(4, 3))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+    plt.title('Correlation Heatmap')
+     
+    plt.tight_layout() 
+    # plt.show()  
+    image_path3 = "static/plot3.png"
+    plt.savefig(image_path3)
+    return (image_path, image_path1, image_path2, image_path3) 
 
 
-
+    
 @app.route('/logout')
 def logout():
-    session.pop('user_id', None)  # Clear the user ID from the session
+    session.pop('user_id', None)  
     return redirect(url_for('home'))
 
 

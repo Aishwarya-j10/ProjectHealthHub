7db476575e258888492ed0d3b183c0a9390c8b0a
Aishwarya <jethani.aishwarya@gmail.com>
2023-09-08 19:57:08 +0530

    added prediction.html

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ from datetime import datetime
 from io import StringIO 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-import pytz 
+import pytz  
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
 
 from sqlalchemy.sql import func
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -220,42 +227,6 @@ def download_health_data_csv():
 
     return redirect(url_for('login'))
 
-# @app.route('/visual_testing')
-# def visual_testing():
-#     user_id = session.get('user_id')
-#     if user_id:
-#         user = User.query.get(user_id)
-#         if user:
-#             health_data = HealthData.query.filter_by(user_id=user_id).all()
-#             if health_data:
-                
-#                 data_list = [
-#                     {
-#                         "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
-#                         "Age": data.age,
-#                         "Gender": data.gender,
-#                         "Heart Rate": data.heart_rate,
-#                         "Anxiety Level": data.anxiety,  
-#                     }
-#                     for data in health_data
-#                 ]
-
-              
-#                 df = pd.DataFrame(data_list)
-#                 file_name = f"user_{user.id}_health_data.csv"
-#                 csv_data = StringIO()
-#                 df.to_csv(csv_data, index=False)
-#                 csv_data.seek(0)
-#                 response = Response(
-#                     csv_data,
-#                     content_type="text/csv",
-#                     headers={
-#                         "Content-Disposition": f"attachment; filename={file_name}"
-#                     }
-#                 )
-#                 return response
-
-    # return redirect(url_for('login'))
 # Addding a new route to download the entire health_data table
 @app.route('/download_entire_health_data_csv')
 def download_entire_health_data_csv():
@@ -302,6 +273,85 @@ def download_entire_health_data_csv():
 def visualizations():
    
     # df = pd.read_csv("user_1.csv") 
+    user_id = session.get('user_id') 
+    print("useridaish", user_id) 
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            health_data = HealthData.query.filter_by(user_id=user_id).all()  
+            if health_data:
+               
+                data_list = [
+                    {
+                        "Date/Time": data.datetime.strftime('%Y-%m-%d %H:%M'), 
+                        "Age": data.age,
+                        "Gender": data.gender,
+                        "Heart Rate": data.heart_rate,
+                        "Anxiety Level": data.anxiety, 
+                    }
+                    for data in health_data
+                ]
+             
+                df = pd.DataFrame(data_list) 
+                df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+    
+                # Filtering data for a specific month (September 2023)
+                start_date = '2023-09-01'
+                end_date = '2023-10-01'
+                df1 = df[(df['Date/Time'] >= start_date) & (df['Date/Time'] < end_date)] 
+    
+                # SORTING THE VALUES IN THE DATAFRAME AND RESETTING THE INDEX:
+                df1 = df1.sort_values(by='Date/Time').reset_index(drop=True)
+                df1.tail()
+                selected_columns = ['Heart Rate', 'Anxiety Level'] 
+                subset_data = df1[selected_columns] 
+                correlation_matrix = subset_data.corr()  
+                # CREATING A TIME SERIES PLOT FOR "Heart Rate"
+                plt.figure(figsize=(6, 3))
+                plt.plot(df1['Date/Time'], df1['Heart Rate'], marker='o', linestyle='-', color='b')
+                plt.title('Heart Rate Over Time')
+                plt.xlabel('Date')
+                plt.ylabel('Heart Rate')
+                plt.grid(True)
+                plt.xticks(rotation=45)
+                plt.tight_layout()   
+                image_path = "static/plot.png"
+                plt.savefig(image_path)
+    
+                # Creating a histogram of anxiety levels for the month: 
+                plt.figure(figsize=(6, 3))
+                plt.hist(df1['Anxiety Level'], bins=10, edgecolor='y')
+                plt.title('Anxiety Level Distribution (September 2023)')
+                plt.xlabel('Anxiety Level')
+                plt.ylabel('Frequency')
+                plt.tight_layout()   
+                image_path1 = "static/plot1.png"
+                plt.savefig(image_path1) 
+                # Create a pie chart
+                category_counts = df1['Heart Rate'].value_counts()
+                plt.figure(figsize=(5, 5))
+                plt.pie(category_counts, labels=category_counts.index, autopct='%1.1f%%', startangle=140)
+                plt.title('Heart Rate Categories')
+                plt.axis('equal')  
+                image_path2 = "static/plot2.png"
+                plt.savefig(image_path2) 
+                selected_columns = ['Heart Rate', 'Anxiety Level'] 
+                subset_data = df1[selected_columns] 
+                correlation_matrix = subset_data.corr()
+
+                # CREATING A HEATMAP TO CHECK HOW ARE ANXIETY AND HEART RATE RELATED: 
+                plt.figure(figsize=(4, 3))
+                sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+                plt.title('Correlation Heatmap')
+     
+                plt.tight_layout() 
+                # plt.show()  
+                image_path3 = "static/plot3.png"
+                plt.savefig(image_path3)
+                return (image_path, image_path1, image_path2, image_path3)  
+
+def dframe() : 
+    # df = pd.read_csv("user_1.csv") 
     user_id = session.get('user_id')
     if user_id:
         user = User.query.get(user_id)
@@ -321,64 +371,114 @@ def visualizations():
                 ]
              
                 df = pd.DataFrame(data_list) 
-    df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+                df['Date/Time'] = pd.to_datetime(df['Date/Time'])
     
-    # Filtering data for a specific month (September 2023)
-    start_date = '2023-09-01'
-    end_date = '2023-10-01'
-    df1 = df[(df['Date/Time'] >= start_date) & (df['Date/Time'] < end_date)] 
+                # Filtering data for a specific month (September 2023)
+                start_date = '2023-09-01'
+                end_date = '2023-10-01'
+                df1 = df[(df['Date/Time'] >= start_date) & (df['Date/Time'] < end_date)] 
     
-    # SORTING THE VALUES IN THE DATAFRAME AND RESETTING THE INDEX:
-    df1 = df1.sort_values(by='Date/Time').reset_index(drop=True)
-    df1.tail()
-    selected_columns = ['Heart Rate', 'Anxiety Level'] 
-    subset_data = df1[selected_columns] 
-    correlation_matrix = subset_data.corr()  
-    # CREATING A TIME SERIES PLOT FOR "Heart Rate"
-    plt.figure(figsize=(6, 3))
-    plt.plot(df1['Date/Time'], df1['Heart Rate'], marker='o', linestyle='-', color='b')
-    plt.title('Heart Rate Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Heart Rate')
-    plt.grid(True)
-    plt.xticks(rotation=45)
-    plt.tight_layout()   
-    image_path = "static/plot.png"
-    plt.savefig(image_path)
-    
-    # Creating a histogram of anxiety levels for the month: 
-    plt.figure(figsize=(6, 3))
-    plt.hist(df1['Anxiety Level'], bins=10, edgecolor='y')
-    plt.title('Anxiety Level Distribution (September 2023)')
-    plt.xlabel('Anxiety Level')
-    plt.ylabel('Frequency')
-    plt.tight_layout()   
-    image_path1 = "static/plot1.png"
-    plt.savefig(image_path1) 
-    # Create a pie chart
-    category_counts = df1['Heart Rate'].value_counts()
-    plt.figure(figsize=(5, 5))
-    plt.pie(category_counts, labels=category_counts.index, autopct='%1.1f%%', startangle=140)
-    plt.title('Heart Rate Categories')
-    plt.axis('equal')  
-    image_path2 = "static/plot2.png"
-    plt.savefig(image_path2) 
-    selected_columns = ['Heart Rate', 'Anxiety Level'] 
-    subset_data = df1[selected_columns] 
-    correlation_matrix = subset_data.corr()
+                # SORTING THE VALUES IN THE DATAFRAME AND RESETTING THE INDEX:
+                df1 = df1.sort_values(by='Date/Time').reset_index(drop=True) 
+                return df1 
 
-    # CREATING A HEATMAP TO CHECK HOW ARE ANXIETY AND HEART RATE RELATED: 
-    plt.figure(figsize=(4, 3))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
-    plt.title('Correlation Heatmap')
-     
-    plt.tight_layout() 
-    # plt.show()  
-    image_path3 = "static/plot3.png"
-    plt.savefig(image_path3)
-    return (image_path, image_path1, image_path2, image_path3) 
+def model1() : 
+    data = pd.read_csv("updated_data1.csv")
 
 
+    # Selecting features and target variable
+    X = data[['Age', 'Heart Rate']].values
+    y = (data['Anxiety Level'] == 'High Anxiety').astype(int)  # Binary classification
+
+    # Splitting the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Standardize features
+    scaler1 = StandardScaler()
+    X_train = scaler1.fit_transform(X_train)
+    X_test = scaler1.transform(X_test)
+
+    # Creating a simple neural network model
+    model11 = keras.Sequential([
+        keras.layers.Input(shape=(X_train.shape[1],)),
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid')  
+        ])
+
+    # Compiling the model
+    model11.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Training the model
+    model11.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=1) 
+
+    # Evaluating the model on the test data
+    # _, accuracy = model11.evaluate(X_test, y_test)
+    # print(f"Test Accuracy: {accuracy}")
+    # scaler = StandardScaler() 
+    # Making predictions
+    # y_pred = model11.predict(X_test)
+    # y_pred_binary = (y_pred > 0.5).astype(int)
+
+    # Generate a classification report
+    # report = classification_report(y_test, y_pred_binary)
+    # print(report) 
+    return model11, scaler1 
+
+def prediction() : 
+    from sklearn.preprocessing import StandardScaler 
+    # df1 = dframe() 
+    model2, scaler1 = model1() 
+    # Creating a DataFrame to hold the new data: 
+    new_age = 21 
+    new_heart_rate = 90 
+    
+    new_data = pd.DataFrame({'Age': [new_age], 'Heart Rate': [new_heart_rate]})
+    # scaler = StandardScaler()
+    # Scaling the new data using the same scaler as used for training data
+    X_new = scaler1.transform(new_data)
+
+    # Making predictions
+    predictions = model2.predict(X_new)
+
+    # Converting the predictions to binary (0 or 1) based on a threshold (e.g., 0.5)
+    predictions_binary = (predictions > 0.5).astype(int) 
+    return predictions_binary 
+
+@app.route('/predicttion', methods=['GET']) 
+def pr() : 
+    # a = prediction() 
+    # df1 = dframe() 
+    model2, scaler1 = model1() 
+    # Creating a DataFrame to hold the new data: 
+    new_age = 21 
+    # new_heart_rate = app.heart_rate 
+    
+    new_data = pd.DataFrame({'Age': [new_age], 'Heart Rate': [app.heart_rate]})
+    # scaler = StandardScaler()
+    # Scaling the new data using the same scaler as used for training data
+    X_new = scaler1.transform(new_data)
+
+    # Making predictions
+    predictions = model2.predict(X_new)
+
+    # Converting the predictions to binary (0 or 1) based on a threshold (e.g., 0.5)
+    a = (predictions > 0.5).astype(int) 
+    
+    return render_template('prediction.html', a = a)
+
+@app.route('/store_heart_rate', methods=['POST'])
+def store_heart_rate():
+    heart_rate = float(request.form.get('heart_rate'))
+    
+    # Store the heart rate in a variable (you can save it to a database here)
+    # For demonstration purposes, we'll store it in a global variable.
+    app.heart_rate = heart_rate
+    
+    return redirect('/prediction')
+
+@app.route('/form', methods=['GET'])
+def show_form():
+    return render_template('prediction.html', a=None)
     
 @app.route('/logout')
 def logout():
